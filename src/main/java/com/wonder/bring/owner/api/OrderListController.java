be10df9c35a51a4d0e8e3477ae58250d9e0b295a
parkdaye <sju02030@navaer.com>
2019-01-04 14:31:09 +0900

    merge orderList

--- src/main/java/com/wonder/bring/owner/api/OrderListController.java
+++ src/main/java/com/wonder/bring/owner/api/OrderListController.java
@@ -1,15 +1,49 @@
 package com.wonder.bring.owner.api;
 
+import com.wonder.bring.owner.service.OrderListsService;
+import com.wonder.bring.owner.utils.Message;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+import static com.wonder.bring.owner.model.DefaultRes.FAIL_DEFAULT_RES;
 
 /**
  * Created by bomi on 2019-01-04.
  */
 
 @Slf4j
-@RequestMapping("orderlists")
+@RequestMapping("")
 @RestController
 public class OrderListController {
+    public final OrderListsService orderListsService;
+
+    public OrderListController(OrderListsService orderListsService) {
+        this.orderListsService = orderListsService;
+    }
+
+    /**
+       주문내역 얻어오기
+     */
+    @GetMapping("/stores/{storeIdx}/orderlists")
+        public ResponseEntity getOrderLists(@PathVariable final int storeIdx) {
+        try {
+            return new ResponseEntity<>(orderListsService.getOrderList(storeIdx), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/orderlists/{orderIdx}")
+    public ResponseEntity changeState( @PathVariable final int orderIdx,
+                                       @RequestParam(value = "state", required = false) final int state) {
+        try {
+            return new ResponseEntity<>(orderListsService.updateOrderState(orderIdx, state), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/com/wonder/bring/owner/utils/Message.java
+++ src/main/java/com/wonder/bring/owner/utils/Message.java
@@ -4,4 +4,6 @@ package com.wonder.bring.owner.utils;
  * Created by bomi on 2019-01-04.
  */
 public class Message {
+    public static final String INTERNAL_SERVER_ERROR = "서버 내부 에러";
+    public static final String DB_ERROR = "데이터베이스 에러";
 }
