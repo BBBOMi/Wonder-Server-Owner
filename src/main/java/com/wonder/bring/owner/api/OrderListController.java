b590c012bf328e68f3664d56a4d0f0eb84026278
bomi <a51022866@gmail.com>
2019-01-04 12:37:42 +0900

    Create

---
+++ src/main/java/com/wonder/bring/owner/api/OrderListController.java
@@ -0,0 +1,15 @@
+package com.wonder.bring.owner.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by bomi on 2019-01-04.
+ */
+
+@Slf4j
+@RequestMapping("orderlists")
+@RestController
+public class OrderListController {
+}
