4f9da99edb422c4fb2be51c61e1116cc54d6d596
bomi <a51022866@gmail.com>
2019-01-08 21:48:14 +0900

    Add Function Push

---
+++ src/main/java/com/wonder/bring/owner/mapper/FcmMapper.java
@@ -0,0 +1,15 @@
+package com.wonder.bring.owner.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * Created by bomi on 2019-01-08.
+ */
+
+@Mapper
+public interface FcmMapper {
+    @Select("SELECT USERS.fcm_token FROM USERS JOIN ORDER_LISTS ON USERS.user_idx = ORDER_LISTS.user_idx WHERE order_idx = #{orderIdx}")
+    String getFcmTokenByOrderIdx(@Param("orderIdx") final int orderIdx);
+}
